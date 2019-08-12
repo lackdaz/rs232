44e31d232c6ca12e8abad3141149fccb8b2cc5bb
ClarkQ1992@gmail.com <ClarkQ1992@gmail.com>
2019-08-12 13:52:54 +0800

    created a function to read the ionCount

--- src/rs232.ino
+++ src/rs232.ino
@@ -2,7 +2,6 @@
 
 SoftwareSerial RS232Out(D4, D3); // RX (red), TX (orange)
 int i = 0;
-int globalIndex = 0;
 
 void setup() {
  Serial.begin(9600);
@@ -12,25 +11,34 @@ void setup() {
 
 void loop()
 {
+  int ion_count = Read_Ion_Count();
+  Serial.println(ion_count);
+}
+int Read_Ion_Count()
+{
  char buffer[4];
- if (RS232Out.available()) 
+ int bufferIndex =0;
+ for( int globalIndex = 0; ;)
  {
-   int inByte = RS232Out.read();
-   if(inByte == 10) 
+   if (RS232Out.available()) 
    {
-    Serial.println("");
-    globalIndex=0;
-   }
-   else if(inByte == 9) 
-   {
-    globalIndex++;
-   }
-   else
-   {
-    if(globalIndex == 3)
-    {
-      Serial.print(char(inByte)); 
-    }
+     int inByte = RS232Out.read();
+     if(inByte == 10) 
+     {
+      break;
+     }
+     else if(inByte == 9)
+     {
+      globalIndex++;
+     }
+     else if(globalIndex == 3)
+     {
+      buffer[bufferIndex] = char(inByte);
+      bufferIndex++;
+     }
    }
  }
+ int  ion_count;
+ ion_count = atoi(buffer);
+ return ion_count;
 }
